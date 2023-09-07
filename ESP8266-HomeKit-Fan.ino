5bf4c7716c026d483629981e98f50da8099428ae
Keeton Feavel <keeton@xyr.is>
2023-09-06 22:44:21 -0400

    Begin work on 315 Mhz receiver

--- ESP8266-HomeKit-Fan.ino
+++ ESP8266-HomeKit-Fan.ino
@@ -22,22 +22,30 @@
 
 #include <Arduino.h>
 #include <arduino_homekit_server.h>
+#include <RCSwitch.h>
 #include "wifi_info.h"
 
 #define LOG_D(fmt, ...)   printf_P(PSTR(fmt "\n") , ##__VA_ARGS__);
 
+static RCSwitch rf315Switch = RCSwitch();
+
 void setup() {
+  // ESP8266 setup
   Serial.begin(115200);
   ets_update_cpu_frequency(160);
+
+  // RF 315 Mhz receiver setup
+  rf315Switch.enableReceive(16);
   
+  // HomeKit setup
   wifi_connect();
   // homekit_storage_reset(); // to remove the previous HomeKit pairing storage when you first run this new HomeKit example
   my_homekit_setup();
 }
 
 void loop() {
+  rf315_loop();
   arduino_homekit_loop();
-  debug_print_heap();
   delay(10);
 }
 
@@ -54,25 +62,25 @@ extern "C" homekit_characteristic_t cha_fan_speed;
 static uint32_t next_heap_millis = 0;
 
 //Called when the switch value is changed by iOS Home APP
-void cha_light_on_setter(const homekit_value_t value) {
+static void cha_light_on_setter(const homekit_value_t value) {
   bool on = value.bool_value;
   cha_light_on.value.bool_value = on;  //sync the value
   LOG_D("Light: %s", on ? "ON" : "OFF");
 }
 
-void cha_fan_on_setter(const homekit_value_t value) {
+static void cha_fan_on_setter(const homekit_value_t value) {
   bool on = value.bool_value;
   cha_fan_on.value.bool_value = on;  //sync the value
   LOG_D("Fan: %s", on ? "ON" : "OFF");
 }
 
-void cha_fan_speed_setter(const homekit_value_t value) {
+static void cha_fan_speed_setter(const homekit_value_t value) {
   uint8_t speed = value.float_value;
   cha_fan_speed.value.float_value = speed;  //sync the value
   LOG_D("Speed: %f", speed);
 }
 
-void my_homekit_setup() {
+static void my_homekit_setup() {
   //Add the .setter function to get the switch-event sent from iOS Home APP.
   //The .setter should be added before arduino_homekit_setup.
   //HomeKit sever uses the .setter_ex internally, see homekit_accessories_init function.
@@ -89,8 +97,19 @@ void my_homekit_setup() {
   //homekit_characteristic_notify(&cha_switch_on, cha_switch_on.value);
 }
 
-void debug_print_heap() {
-#ifdef DEBUG_HEAP
+static void rf315_loop() {
+  if (rf315Switch.available()) {
+    unsigned long value = rf315Switch.getReceivedValue();
+
+    digitalWrite(LED_BUILTIN, LOW);
+    LOG_D("Recieved value: 0x%X", value);
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    rf315Switch.resetAvailable();
+  }
+}
+
+static void debug_print_heap() {
   const uint32_t t = millis();
   if (t > next_heap_millis) {
     // show heap info every 5 seconds
@@ -98,5 +117,4 @@ void debug_print_heap() {
     LOG_D("Free heap: %d, HomeKit clients: %d",
         ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
   }
-#endif
 }
